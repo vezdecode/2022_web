5f7ddd4c8fdb978f029bebee5a21a5618ba69e42
Kirill Osintsev <osintsev.2000@inbox.ru>
2022-04-09 23:30:28 +0500

    fixed dnd

--- store/dnd.js
+++ store/dnd.js
@@ -7,12 +7,22 @@ export const mutations = {
 		state.cardsPositions = cards;
 	},
 	moveCard (state, cardsIds) {
-		state.cardsPositions.splice(state.cardsPositions.indexOf(+cardsIds[0]), 1);
-		state.cardsPositions = [
-			...state.cardsPositions.slice(0, state.cardsPositions.indexOf(+cardsIds[1])),
-			+cardsIds[0],
-			...state.cardsPositions.slice(state.cardsPositions.indexOf(+cardsIds[1])),
-		];
+		if(state.cardsPositions.indexOf(+cardsIds[1]) < state.cardsPositions.indexOf(+cardsIds[0])) {
+			state.cardsPositions.splice(state.cardsPositions.indexOf(+cardsIds[0]), 1);
+			state.cardsPositions = [
+				...state.cardsPositions.slice(0, state.cardsPositions.indexOf(+cardsIds[1])),
+				+cardsIds[0],
+				...state.cardsPositions.slice(state.cardsPositions.indexOf(+cardsIds[1])),
+			];
+		}
+		else {
+			state.cardsPositions.splice(state.cardsPositions.indexOf(+cardsIds[0]), 1);
+			state.cardsPositions = [
+				...state.cardsPositions.slice(0, state.cardsPositions.indexOf(+cardsIds[1]) + 1),
+				+cardsIds[0],
+				...state.cardsPositions.slice(state.cardsPositions.indexOf(+cardsIds[1]) + 1),
+			];
+		}
 		localStorage.setItem('cards_pos', JSON.stringify(state.cardsPositions));
 	},
 };
